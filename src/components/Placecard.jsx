df6a9215b5201367507eba58653ab93946c8ab60
jeanmax1me <jeanmaximegilbert@gmail.com>
2023-08-08 12:46:20 +0200

    some fixes

--- src/components/Placecard.jsx
+++ src/components/Placecard.jsx
@@ -1,8 +1,10 @@
 
 
 const PlaceCard = ({ place }) => {
-  const randomImageNumber = Math.floor(Math.random() * 7) + 1; 
-  const imageUrl = `/room${randomImageNumber}.jpg`; 
+  const randomImageNumber = Math.floor(Math.random() * 7) + 1;
+  const imageUrl = `/room${randomImageNumber}.jpg`;
+  const bookingAffiliateLink = "https://www.booking.com/index.html?aid=8066595";
+
   return (
     <div className="bg-white rounded-lg shadow-md p-4 mb-4 flex flex-row">
       <img src={imageUrl} alt={place.title} className="w-44 aspect-[1.052/1] object-cover mb-2" />
@@ -12,7 +14,12 @@ const PlaceCard = ({ place }) => {
         <p className="text-indigo-600 font-semibold mb-1">{place.rating} / 10</p>
         <p className="text-gray-700 mb-2">Average Price: {place.price} per night</p>
       </div>
-      <a href={place.bookingLink} target="_blank" rel="noopener noreferrer" className="text-indigo-600 hover:underline transition duration-300">
+      <a
+        href={bookingAffiliateLink}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-indigo-600 hover:underline transition duration-300"
+      >
         Book Now on Booking.com
       </a>
     </div>
