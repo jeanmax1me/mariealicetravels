febd0233c5310eca60a680a6fead285b9d87c9fa
jeanmax1me <jeanmaximegilbert@gmail.com>
2023-08-08 15:24:36 +0200

    newbuild

--- src/components/RedirectToBookingPage.jsx
+++ src/components/RedirectToBookingPage.jsx
@@ -4,13 +4,18 @@ import redirects from './redirects.json'; // Import the JSON configuration
 
 export default function RedirectToBookingPage() {
   const { propertyId } = useParams(); // Get the property ID from the URL parameter
+
   useEffect(() => {
+    console.log('RedirectToBookingPage component triggered'); // Log that the component is triggered
+
     if (propertyId && redirects[propertyId]) {
+      console.log('Redirecting to:', redirects[propertyId]); // Log the redirection URL
       // Asynchronously redirect to the Booking.com URL from the configuration
       setTimeout(() => {
         window.location.replace(redirects[propertyId]);
       }, 0);
     } else {
+      console.log('Property ID not found or redirection URL not available');
       // Redirect to Booking.com main page with your affiliate ID
       setTimeout(() => {
         window.location.replace(`https://www.booking.com/index.html?aid=8066595`);
