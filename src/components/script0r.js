3a2fca71fd69442e8b8a6f5063bf58199121cec7
jeanmax1me <jeanmaximegilbert@gmail.com>
2023-08-09 22:26:04 +0200

    gsgsd

--- src/components/script0r.js
+++ src/components/script0r.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 
 let inputString = 
-"https://www.booking.com/hotel/fr/superbe-t2-climatise-au-centre-parking.fr.html?aid=356980&label=gog235jc-1FCAMoTUIGY2FubmVzSDNYA2hNiAEBmAENuAEXyAEM2AEB6AEB-AEDiAIBqAIDuAL2xc-mBsACAdICJGFlYTMyZDBhLWNiMTAtNDVjMi04YTg3LThjZTU0N2Y4OWNjNdgCBeACAQ&sid=03d70092256621f8da17e3e952131a1f&all_sr_blocks=949881201_367635663_2_0_0&checkin=2023-09-30&checkout=2023-10-01&dest_id=-1416533&dest_type=city&dist=0&group_adults=1&group_children=0&hapos=45&highlighted_blocks=949881201_367635663_2_0_0&hpos=20&matching_block_id=949881201_367635663_2_0_0&nflt=privacy_type%3D3%3Bht_id%3D201&no_rooms=1&req_adults=1&req_children=0&room1=A&sb_price_type=total&sr_order=upsort_bh&sr_pri_blocks=949881201_367635663_2_0_0__14114&srepoch=1691607869&srpvid=2e40860797900026&type=total&ucfs=1&activeTab=photosGallery"
+"https://www.booking.com/hotel/fr/le-leo-600m-plage-exterieur-verriere-cote-d-azur.fr.html?aid=356980&label=gog235jc-1FCAMoTUIGY2FubmVzSDNYA2hNiAEBmAENuAEXyAEM2AEB6AEB-AEDiAIBqAIDuAL2xc-mBsACAdICJGFlYTMyZDBhLWNiMTAtNDVjMi04YTg3LThjZTU0N2Y4OWNjNdgCBeACAQ&sid=03d70092256621f8da17e3e952131a1f&all_sr_blocks=882805302_357600630_2_0_0&checkin=2023-10-01&checkout=2023-10-02&dest_id=-1416533&dest_type=city&dist=0&group_adults=1&group_children=0&hapos=58&highlighted_blocks=882805302_357600630_2_0_0&hpos=8&matching_block_id=882805302_357600630_2_0_0&nflt=privacy_type%3D3%3Bht_id%3D201&no_rooms=1&req_adults=1&req_children=0&room1=A&sb_price_type=total&sr_order=upsort_bh&sr_pri_blocks=882805302_357600630_2_0_0__11521&srepoch=1691611521&srpvid=2e40860797900026&type=total&ucfs=1&activeTab=photosGallery"
 
 
 let indexOfHtml = inputString.indexOf("html?");
